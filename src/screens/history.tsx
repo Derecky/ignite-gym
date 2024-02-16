36cd327cc2aaade404a8268de46bc288a114d439
Derecky Costa <dereckycosta@gmail.com>
2024-02-16 00:24:45 -0300

    feat: create history screen layout

--- src/screens/history.tsx
+++ src/screens/history.tsx
@@ -1,9 +1,39 @@
-import { Center, Text } from "native-base";
+import { HistoryCard } from "@/components/history-card";
+import { ScreenHeader } from "@/components/screen-header";
+import { Heading, VStack, SectionList, Text, Center } from "native-base";
+import { useState } from "react";
 
-export function History () {
+export function History() {
+  const [exercises, setExercises] = useState([
+    { title: "24/10/2023", data: ["puxada frontal", "remada unilateral"] },
+    { title: "23/10/2023", data: ["remada unilateral"] },
+  ]);
+
   return (
-    <Center flex={1}>
-      <Text color="white">History</Text>
-    </Center>
-  )
+    <VStack flex={1}>
+      <ScreenHeader title="Histórico de exercícios" />
+
+      <SectionList
+        sections={exercises}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <HistoryCard />}
+        renderSectionHeader={({ section }) => (
+          <Heading color="gray.200" fontSize="md" mt={10} mb={3}>
+            {section.title}
+          </Heading>
+        )}
+        px={8}
+        contentContainerStyle={
+          exercises.length === 0 && { flex: 1, justifyContent: "center" }
+        }
+        ListEmptyComponent={() => (
+          <Text color="gray.100" textAlign="center">
+            Não há exercícios registrados ainda.{"\n"} Vamos fazer exercícios
+            hoje?
+          </Text>
+        )}
+        showsVerticalScrollIndicator={false}
+      />
+    </VStack>
+  );
 }
